e24d6d772327ea3eda9262703d352737af3410d0
Roninshadow <vadimcornex@gmail.com>
2025-05-19 05:14:06 +0300

    Digital

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,9 @@
 const links = document.querySelectorAll('.header__nav-link')
 const page = window.location.pathname.split('/').pop()
 const faqs = document.querySelectorAll('.faq__item')
+const dialog = document.getElementById('dialog')
+const openDialog = document.getElementById('open')
+const closeDialog = document.getElementById('close')
 
 faqs.forEach(faq => {
 	faq.addEventListener('click', () => {
@@ -20,3 +23,38 @@ links.forEach(item => {
 		item.classList.add('active')
 	}
 })
+
+
+function openPopup () {
+	dialog.showModal()
+	document.body.classList.add('scroll-block')
+}
+
+function returnScroll() {
+	document.body.classList.remove('scroll-block')
+}
+
+function closePopup() {
+	dialog.close()
+	returnScroll()
+}
+function closeOverlay ({ currentTarget, target }) {
+	const dialog = currentTarget
+	const OverlayClick = target === dialog 
+	if (OverlayClick) {
+		closePopup()
+	}
+}
+
+openDialog.addEventListener('click', () => {
+	openPopup()
+})
+closeDialog.addEventListener('click', () => {
+	closePopup()
+})
+dialog.addEventListener('click', (e) => {
+	closeOverlay(e)
+})
+dialog.addEventListener('cancel', () => {
+	returnScroll()
+});
